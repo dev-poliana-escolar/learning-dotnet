4004d1543600f51aa26c95cb0abf3893d1fcc9b0
dev-poliana-escolar <poliana.pinheiro@escolar.ifrn.edu.br>
2026-05-11 14:19:48 -0300

    feat: model e controller

---
+++ gerencia_alunos/Models/Aluno.cs
@@ -0,0 +1,10 @@
+﻿namespace gerencia_alunos;
+
+public class Aluno
+{
+    public int Id {get; set;}
+    public string Nome {get; set;} = string.Empty;
+    public string Email {get; set;} = string.Empty;
+    public string Curso {get; set;} = string.Empty;
+    public date DataNascimento {get;set;}=  string.Empty;
+}
