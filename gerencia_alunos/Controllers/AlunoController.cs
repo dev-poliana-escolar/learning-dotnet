f31cdc58fa1357e3bf247413a2a4269d440d822a
dev-poliana-escolar <poliana.pinheiro@escolar.ifrn.edu.br>
2026-05-16 12:40:19 -0300

    feat: get, post e post por id de aluno

--- gerencia_alunos/Controllers/AlunoController.cs
+++ gerencia_alunos/Controllers/AlunoController.cs
@@ -1,11 +1,56 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using gerencia_alunos.Data;
+using gerencia_alunos.Models;
+using Microsoft.AspNetCore.Authorization;
+ 
 
 namespace gerencia_alunos.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class AlunoController : ControllerBase
     {
+        private readonly AppDbContext _context;
+        public AlunoController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Aluno> Get()
+        {
+            return _context.Alunos.ToList();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CriarAluno(Aluno aluno)
+        {
+            await _context.Alunos.AddAsync(aluno);
+
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(
+                nameof(BuscarPorId),
+                new { id = aluno.Id },
+                aluno
+            );
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> BuscarPorId(int id)
+        {
+            var aluno = await _context.Alunos.FindAsync(id);
+
+            if (aluno == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(aluno);
+        }
+
+        
     }
 }
