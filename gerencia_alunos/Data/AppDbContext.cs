1f988b93fb5f6b72c33420b2b7487c153a27fec0
dev-poliana-escolar <poliana.pinheiro@escolar.ifrn.edu.br>
2026-05-16 11:37:27 -0300

    build: Add Entity Framework Core e configuração do contexto de banco de dados Ao escolher usar docker ao invés de SQL Server local, foi necessário adicionar o pacote Microsoft.EntityFrameworkCore.Design para permitir a criação das migrações e configuração do contexto de banco de dados. O appsettings.json foi atualizado para incluir a string de conexão com o banco de dados SQL Server rodando no Docker.

---
+++ gerencia_alunos/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using gerencia_alunos.Models;
+
+namespace gerencia_alunos.Data
+{   
+
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Aluno> Alunos { get; set; }
+    }
+}
