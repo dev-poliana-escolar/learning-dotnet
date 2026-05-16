3803d9ecb97d932e8f386d411b5719a5d66ea68f
dev-poliana-escolar <poliana.pinheiro@escolar.ifrn.edu.br>
2026-05-16 13:13:13 -0300

    refactor: get assincrono

--- gerencia_alunos/Controllers/AlunoController.cs
+++ gerencia_alunos/Controllers/AlunoController.cs
@@ -3,7 +3,8 @@ using Microsoft.AspNetCore.Mvc;
 using gerencia_alunos.Data;
 using gerencia_alunos.Models;
 using Microsoft.AspNetCore.Authorization;
- 
+using Microsoft.EntityFrameworkCore;
+
 
 namespace gerencia_alunos.Controllers
 {
@@ -19,11 +20,11 @@ namespace gerencia_alunos.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<Aluno> Get()
+        public async Task<ActionResult<IEnumerable<Aluno>>> Get()
         {
-            return _context.Alunos.ToList();
+            return await _context.Alunos.ToListAsync();
         }
-
+    
         [HttpPost]
         public async Task<IActionResult> CriarAluno(Aluno aluno)
         {
@@ -49,6 +50,49 @@ namespace gerencia_alunos.Controllers
             }
 
             return Ok(aluno);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> AtualizarAluno(int id, Aluno aluno)
+        {
+            if (id != aluno.Id) // se id diferente do id do objeto
+            {
+                return BadRequest("O ID da URL é diferente do ID do objeto.");
+            }
+
+            var alunoExistente = await _context.Alunos.FindAsync(id); // busca o aluno
+
+            if (alunoExistente == null)
+            {
+                return NotFound();
+            }
+
+            // atualiza as propriedades do aluno existente com os valores do aluno recebido
+            alunoExistente.Nome = aluno.Nome;
+            alunoExistente.Email = aluno.Email;
+            alunoExistente.Curso = aluno.Curso;
+            alunoExistente.DataNascimento = aluno.DataNascimento;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(alunoExistente);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletarAluno(int id)
+        {
+            var aluno = await _context.Alunos.FindAsync(id);
+
+            if (aluno == null)
+            {
+                return NotFound();
+            }
+
+            _context.Alunos.Remove(aluno);
+
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
 
         
